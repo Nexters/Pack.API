145e8632b036064cc341d4dfe0606d6216a8177d
skylershin <munkyu.s.shin@gmail.com>
2014-07-24 15:46:10 +0900

    stations angular controller 역 목록 가져오는 테스트 코드 작성. 확인은 아직.

--- packages/stations/public/tests/stations.spec.js
+++ packages/stations/public/tests/stations.spec.js
@@ -3,7 +3,24 @@
 (function() {
   describe('MEAN controllers', function() {
         describe('StationsController', function() {
+          var scope, ctrl, $mockBackend;
 
+          beforeEach(inject(function(_$httpBackend_, $rootScope, $controller) {
+            $monckBackend = _$httpBackend_;
+            $mockBackend.expectGET('stations/').
+                respond([{name:'신세이비조 역'}, {name:'세이비조 역'}]);
+
+            scope = $rootScope.$new();
+            ctrl = $controller('StationsController', {$scope: scope});
+          }));
+
+          it('stations 모델 두개가 fetch 한 것과 동일해야 함', function() {
+            expect(scope.stations).toEqualData([]);
+            $mockBackend.flush();
+
+            expect(scope.stations).toEqualData(
+              [{name:'신세이비조 역'}, {name:'세이비조 역'}]);
+          });
         });
   });
 }());
