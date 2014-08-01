07d76d7434cde34ee522970af9cff2f31ae5eac7
Rangken <rangken@gmail.com>
2014-08-01 16:02:52 +0900

    문규 에러나는거 정리:

--- packages/guesthouses/server/routes/guesthouses.js
+++ packages/guesthouses/server/routes/guesthouses.js
@@ -3,14 +3,5 @@
 // The Package is past automatically as first parameter
 module.exports = function(Guesthouses, app, auth, database) {
 
-    app.route('/guesthouses')
-        .get(stations.all)
-        .post(stations.create);
-  
-    app.route('/guesthouses/:stationId')
-        .put(stations.update)
-        .get(stations.show);
-
-    app.param('stationId', stations.station);
 
 };
